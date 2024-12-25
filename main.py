b0db1ade1d1ad1ed188ad2012477b6aa09bcd566
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-12-26 00:02:39 +0530

    Initializing main.py for model evaluation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.pipeline.model_training_pipeline import ModelTrainerPipeline
+from src.pipeline.model_evaluation_pipeline import ModelEvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -40,6 +41,16 @@ try:
       logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
       data_transformation = ModelTrainerPipeline()
       data_transformation.inititate_model_training()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+      logger.exception(e)
+      raise e
+
+STAGE_NAME = "Model Evalution stage"
+try:
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+      data_transformation = ModelEvaluationPipeline()
+      data_transformation.inititate_model_eval()
       logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
       logger.exception(e)
